0d7489315f95b865a42ab6cd224a93a6c130bba0
Minh Quan <minhquancn1809@gmail.com>
2022-09-13 23:32:28 +0700

    fix tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
   "./node_modules/flowbite-react/**/*.js",
   "./pages/**/*.{ts,tsx}",
   "./public/**/*.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
  ],
  theme: {
   extend: {},
  },
- plugins: [require("flowbite/plugin")],
+ plugins: [
+  require("flowbite/plugin"),
+  require("@tailwindcss/forms"),
+  require("@tailwindcss/aspect-ratio"),
+ ],
 };
