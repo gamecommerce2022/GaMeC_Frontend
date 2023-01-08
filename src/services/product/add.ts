41e524445e6445059cde90bb1b807b189e9fa120
thinhhja2001 <19522281@gm.uit.edu.vn>
2023-01-09 00:27:39 +0700

    update product in browse

--- src/services/product/add.ts
+++ src/services/product/add.ts
@@ -1,22 +1,22 @@
-import axios from "axios";
-import Cookies from "js-cookie";
-import { Product } from "../../model/product_model";
-import { productUrl } from "../url";
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { Product } from '../../model/product_model';
+import { productUrl } from '../url';
 
-const token = Cookies.get('accessToken')
+const token = Cookies.get('accessToken');
 const config = {
- headers: { Authorization: `Bearer ${token}` }
+  headers: { Authorization: `Bearer ${token}` },
 };
 
+export const addGame: (game: Product) => Promise<string | null> = async (game: Product) => {
+  console.log('game data is ', game);
 
-
-export const addGame : (game: Product) => Promise<string | null> = async (game: Product) => {
-    let res = await axios.post(`${productUrl}`, game, config);
-    console.log(res)
-    let code = res.data.code;
-    if(code === 200){
-        return res.data.id;
-    } else {
-        return null;
-    }
-}
+  let res = await axios.post(`${productUrl}`, game, config);
+  console.log(res);
+  let code = res.data.code;
+  if (code === 200) {
+    return res.data.id;
+  } else {
+    return null;
+  }
+};
--- src/ui/user/browser/category/component/product_item.tsx
+++ src/ui/user/browser/category/component/product_item.tsx
@@ -13,42 +13,44 @@ interface ProductItemProps {
 export const ProductItem: React.FC<ProductItemProps> = (props) => {
   const navigate = useNavigate();
   return (
-    <div
-      className="w-full h-max flex flex-col"
-      onClick={() => {
-        navigate(`/user/products/${props.id}`);
-      }}
-    >
-      <img className="w-full h-64 object-fill rounded" src={props.img} alt={props.name} />
-      <div className="text-gray-500 mt-2">{props.type}</div>
-      <div>
-        <h1 className="text-white text-xl">{props.name}</h1>
-      </div>
-      <div className="flex-grow"></div>
-      <div className="flex flex-row justify-between items-center">
-        {props.discount > 0 && (
-          <span className="text-white bg-blue-700 rounded-lg">
-            <p className="m-2 text-base">-{props.discount * 100}%</p>
-          </span>
-        )}
+    <a href={`/user/products/${props.id}`}>
+      <div
+        className="w-full h-max flex flex-col"
+        onClick={() => {
+          navigate(`/user/products/${props.id}`);
+        }}
+      >
+        <img className="w-full h-64 object-fill rounded" src={props.img} alt={props.name} />
+        <div className="text-gray-500 mt-2">{props.type}</div>
         <div>
-          <div
-            className={classNames(
-              'text-base',
-              props.discount > 0
-                ? 'line-through text-gray-400 font-medium'
-                : 'text-gray-500 font-bold',
-            )}
-          >
-            {props.price > 0 ? `${withCurrency(props.price)}` : 'Free'}
-          </div>
+          <h1 className="text-white text-xl">{props.name}</h1>
+        </div>
+        <div className="flex-grow"></div>
+        <div className="flex flex-row justify-between items-center">
           {props.discount > 0 && (
-            <h3 className="text-white text-base font-bold">
-              {withCurrency(discountCalc(props.discount, props.price))}
-            </h3>
+            <span className="text-white bg-blue-700 rounded-lg">
+              <p className="m-2 text-base">-{props.discount * 100}%</p>
+            </span>
           )}
+          <div>
+            <div
+              className={classNames(
+                'text-base',
+                props.discount > 0
+                  ? 'line-through text-gray-400 font-medium'
+                  : 'text-gray-500 font-bold',
+              )}
+            >
+              {props.price > 0 ? `${withCurrency(props.price)}` : 'Free'}
+            </div>
+            {props.discount > 0 && (
+              <h3 className="text-white text-base font-bold">
+                {withCurrency(discountCalc(props.discount, props.price))}
+              </h3>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
